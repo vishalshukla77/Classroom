60fc2be3d8011cbe5a18a1354ceccf9006c2d6eb
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2024-08-12 19:25:42 +0530

    Update teacherRoutes.js

--- backend/routes/teacherRoutes.js
+++ backend/routes/teacherRoutes.js
@@ -1,11 +1,1 @@
-// routes/teacherRoutes.js
-const express = require('express');
-const { createTimetable } = require('../controllers/teacherController');
-const { protect, authorize } = require('../middleware/authMiddleware');
-
-
-const router = express.Router();
-
-router.post('/timetable', protect, authorize('Teacher'), createTimetable);
-
-module.exports = router;
+// routes/teacherRoute for timetable to be added later
